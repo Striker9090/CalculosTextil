2a963bc5a0c5aaab8beb463f9ee026e3a569d4d9
Kaue <kaueerosane@gmail.com>
2025-06-08 18:20:47 -0300

    Redirects

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,19 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import {Fiacao} from '../pages/01_fiacao'; // ou o caminho correto
-import App from '../pages/00_index'
-import { Tecelagem } from '../pages/02_tecelagem';
-import { Malharia } from '../pages/03_malharia';
-import { Beneficiamento } from '../pages/04_beneficiamento';
+import { Routes, Route, Link } from "react-router-dom";
+import { Fiacao } from "../pages/01_fiacao"; // ou o caminho correto
+import App from "../pages/00_index";
+import { Tecelagem } from "../pages/02_tecelagem";
+import { Malharia } from "../pages/03_malharia";
+import { Beneficiamento } from "../pages/04_beneficiamento";
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path="/fiacao" element={<Fiacao />} /> {/* <-- Aqui está sua rota */}
-      <Route path="/tecelagem" element={<Tecelagem />} /> {/* <-- Aqui está sua rota */}
-      <Route path="/malharia" element={<Malharia />} /> {/* <-- Aqui está sua rota */}
-      <Route path="/beneficiamento" element={<Beneficiamento />} /> {/* <-- Aqui está sua rota */}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/fiacao" element={<Fiacao />} />
+        <Route path="/tecelagem" element={<Tecelagem />} />
+        <Route path="/malharia" element={<Malharia />} />
+        <Route path="/beneficiamento" element={<Beneficiamento />} />
+      </Routes>
   );
 }
 
@@ -22,10 +22,10 @@ type RoutesLinkProps = {
   children: React.ReactNode;
 };
 
-export function RoutesLink({to, children}: RoutesLinkProps){
-    return(
-        <>
-            <Link to={to}>{children}</Link>
-        </>
-    )
+export function RoutesLink({ to, children }: RoutesLinkProps) {
+  return (
+    <>
+      <Link to={to}>{children}</Link>
+    </>
+  );
 }
